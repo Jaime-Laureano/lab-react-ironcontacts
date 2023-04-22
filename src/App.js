5853f296cec69138139da9d7dd9fd5a3453e17e9
Jaime-Laureano <96260804+Jaime-Laureano@users.noreply.github.com>
2023-04-22 13:27:54 +0100

    Completed lab for students

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+// this solution uses bootstrap classes
+// added <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" > to index.html
+
+import React from "react";
+import "./App.css";
+import ContactsList from "./components/ContactsList";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ContactsList />
     </div>
   );
 }
